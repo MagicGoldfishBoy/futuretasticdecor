7262946b3522c002cf6b1c3a875b73ee3fa72b0e
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-10-07 16:21:05 -0400

    places flowers in all directions

--- src/main/java/com/magicgoldfishboy/futuretasticdecor/block/SuperGrowMulch.java
+++ src/main/java/com/magicgoldfishboy/futuretasticdecor/block/SuperGrowMulch.java
@@ -2,6 +2,7 @@ package com.magicgoldfishboy.futuretasticdecor.block;
 
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
+import java.util.Random;
 import javax.annotation.Nonnull;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -9,7 +10,9 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FlowerBlock;
+import net.minecraft.world.level.block.TallFlowerBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 
 public class SuperGrowMulch extends Block {
     
@@ -19,27 +22,22 @@ public class SuperGrowMulch extends Block {
     
     public static final Logger LOGGER = LogUtils.getLogger();
     private static final int SPREAD_INTERVAL = 20; // 1 second (20 ticks)
+    Random randomDirectionNumber = new Random();
     
     @Override
     public void onPlace(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos, @Nonnull BlockState oldState, boolean isMoving) {
         super.onPlace(state, level, pos, oldState, isMoving);
-        LOGGER.info("onPlace called at {} (isClientSide: {}, oldState: {})", pos, level.isClientSide(), oldState.getBlock());
         
         if (!level.isClientSide()) {
-            // Always schedule a tick when placed (not when updating)
             if (!oldState.is(this)) {
-                LOGGER.info("Scheduling initial tick at {}", pos);
-                level.scheduleTick(pos, this, 1); // Start immediately
+                level.scheduleTick(pos, this, 1);
             }
             
-            // Also notify neighboring mulch blocks to start ticking if they have flowers
             for (net.minecraft.core.Direction direction : net.minecraft.core.Direction.Plane.HORIZONTAL) {
                 BlockPos neighborPos = pos.relative(direction);
                 BlockState neighborState = level.getBlockState(neighborPos);
-                LOGGER.info("Checking {} neighbor at {}: {}", direction, neighborPos, neighborState.getBlock());
+                //LOGGER.info("Checking {} neighbor at {}: {}", direction, neighborPos, neighborState.getBlock());
                 if (neighborState.getBlock() instanceof SuperGrowMulch) {
-                    // Restart the neighbor's tick cycle
-                    LOGGER.info("Found neighboring mulch, scheduling tick");
                     level.scheduleTick(neighborPos, neighborState.getBlock(), 1);
                 }
             }
@@ -48,34 +46,94 @@ public class SuperGrowMulch extends Block {
     
     @Override
     public void tick(@Nonnull BlockState state, @Nonnull ServerLevel level, @Nonnull BlockPos pos, @Nonnull RandomSource random) {
-        //LOGGER.info("supermulch ticked at {}", pos);
         
         BlockState aboveState = level.getBlockState(pos.above());
-        //LOGGER.info("Block above: {}", aboveState.getBlock());
         
-        if (aboveState.getBlock() instanceof FlowerBlock) {
-            //LOGGER.info("Found flower above!");
+        if (aboveState.getBlock() instanceof FlowerBlock || aboveState.getBlock() instanceof TallFlowerBlock) {
+            int randomDirectionNumber1 = randomDirectionNumber.nextInt(5);
+            switch (randomDirectionNumber1) {
+                case 1: checkEastForMulch(state, level, pos, random);
+                    
+                    break;
+
+                case 2: checkWestForMulch(state, level, pos, random);
+
+                    break;
+
+                case 3: checkNorthForMulch(state, level, pos, random);
+
+                    break;
+
+                case 4: checkSouthForMulch(state, level, pos, random);
+
+                    break;
             
-            BlockState eastState = level.getBlockState(pos.east());
-            //LOGGER.info("Block to east: {}", eastState.getBlock());
+                default: checkEastForMulch(state, level, pos, random);
+                    break;
+            }  
+        }
+
+        level.scheduleTick(pos, this, SPREAD_INTERVAL);
+    }    
+    
+    private void checkEastForMulch(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        BlockState eastState = level.getBlockState(pos.east());
+        
+        if (eastState.getBlock() instanceof SuperGrowMulch) {
             
-            if (eastState.getBlock() instanceof SuperGrowMulch) {
-                //LOGGER.info("Found mulch to east!");
-                
-                BlockPos targetPos = pos.east().above();
-                BlockState targetState = level.getBlockState(targetPos);
-                //LOGGER.info("Target position: {}, Target block: {}", targetPos, targetState.getBlock());
-                
-                if (targetState.isAir()) {
-                    LOGGER.info("Target is air, placing flower!");
-                    level.setBlock(targetPos, aboveState, 3);
-                } else {
-                    LOGGER.info("Target is not air");
-                }
+            BlockPos targetPos = pos.east().above();
+            BlockState targetState = level.getBlockState(targetPos);
+            
+            if (targetState.isAir()) {
+                level.setBlock(targetPos, level.getBlockState(pos.above()), 3);
+                // if (level.getBlockState(pos.above()).getBlock() instanceof TallFlowerBlock) {
+                //     level.setBlock(pos, level.getBlockState(pos.above()) state.setValue(TallFlowerBlock.HALF, DoubleBlockHalf.UPPER, 0));
+                // }
+            } else {
             }
         }
-        
-        // Schedule next tick
-        level.scheduleTick(pos, this, SPREAD_INTERVAL);
     }
+    private void checkWestForMulch(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        BlockState westState = level.getBlockState(pos.west());
+        
+        if (westState.getBlock() instanceof SuperGrowMulch) {
+            
+            BlockPos targetPos = pos.west().above();
+            BlockState targetState = level.getBlockState(targetPos);
+            
+            if (targetState.isAir()) {
+                level.setBlock(targetPos, level.getBlockState(pos.above()), 3);
+            } else {
+            }
+        }
+    }
+    private void checkNorthForMulch(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        BlockState northState = level.getBlockState(pos.north());
+        
+        if (northState.getBlock() instanceof SuperGrowMulch) {
+            
+            BlockPos targetPos = pos.north().above();
+            BlockState targetState = level.getBlockState(targetPos);
+            
+            if (targetState.isAir()) {
+                level.setBlock(targetPos, level.getBlockState(pos.above()), 3);
+            } else {
+            }
+        }
+    }
+    private void checkSouthForMulch(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        BlockState southState = level.getBlockState(pos.south());
+        
+        if (southState.getBlock() instanceof SuperGrowMulch) {
+            
+            BlockPos targetPos = pos.south().above();
+            BlockState targetState = level.getBlockState(targetPos);
+            
+            if (targetState.isAir()) {
+                level.setBlock(targetPos, level.getBlockState(pos.above()), 3);
+            } else {
+            }
+        }
+    }
+
 }
