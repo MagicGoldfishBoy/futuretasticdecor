96f4a7930e3e8ddba8682e20106c3a7ea6fb902c
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-10-16 17:05:11 -0400

    arrow fills up all the way

--- src/main/java/com/magicgoldfishboy/futuretasticdecor/block/entity/HolocookerEntity.java
+++ src/main/java/com/magicgoldfishboy/futuretasticdecor/block/entity/HolocookerEntity.java
@@ -23,8 +23,18 @@ import net.minecraft.world.level.block.entity.FuelValues;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class HolocookerEntity extends AbstractFurnaceBlockEntity {
-    public static final Logger LOGGER = LogUtils.getLogger();
     private static final Component DEFAULT_NAME = Component.translatable("container.smoker");
+    
+    private static final java.lang.reflect.Field COOKING_TOTAL_TIME_FIELD;
+    
+    static {
+        try {
+            COOKING_TOTAL_TIME_FIELD = AbstractFurnaceBlockEntity.class.getDeclaredField("cookingTotalTime");
+            COOKING_TOTAL_TIME_FIELD.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public HolocookerEntity(BlockPos pos, BlockState blockState) {
         super(LaboratoryDecorRegistry.HOLOCOOKER_ENTITY.get(), pos, blockState, RecipeType.SMOKING);
@@ -37,47 +47,32 @@ public class HolocookerEntity extends AbstractFurnaceBlockEntity {
 
     @Override
     protected int getBurnDuration(FuelValues fuelValues, ItemStack stack) {
-        return (super.getBurnDuration(fuelValues, stack) * 4);
+        return (super.getBurnDuration(fuelValues, stack) / 4);
     }
+
     @Override
     protected AbstractContainerMenu createMenu(int id, Inventory player) {
         return new SmokerMenu(id, player, this, this.dataAccess);
     }
-
-    private static final java.lang.reflect.Field COOKING_TIMER_FIELD;
-    private static final java.lang.reflect.Field COOKING_TOTAL_TIME_FIELD;
-    private static final java.lang.reflect.Field LIT_TIME_REMAINING_FIELD;
-
-    static {
-        try {
-            COOKING_TIMER_FIELD = AbstractFurnaceBlockEntity.class.getDeclaredField("cookingTimer");
-            COOKING_TOTAL_TIME_FIELD = AbstractFurnaceBlockEntity.class.getDeclaredField("cookingTotalTime");
-            LIT_TIME_REMAINING_FIELD = AbstractFurnaceBlockEntity.class.getDeclaredField("litTimeRemaining");
-            
-            COOKING_TIMER_FIELD.setAccessible(true);
-            COOKING_TOTAL_TIME_FIELD.setAccessible(true);
-            LIT_TIME_REMAINING_FIELD.setAccessible(true);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
+    
+    @Override
+    public void setItem(int index, ItemStack stack, boolean insideTransaction) {
+        super.setItem(index, stack, insideTransaction);
+        
+        // Scale down the cooking total time when a new item is set
+        if (index == 0) {
+            try {
+                int totalTime = COOKING_TOTAL_TIME_FIELD.getInt(this);
+                if (totalTime > 0) {
+                    COOKING_TOTAL_TIME_FIELD.setInt(this, Math.max(1, totalTime / 4));
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void serverTick(ServerLevel level, BlockPos pos, BlockState state, HolocookerEntity furnace) {
-        try {
-            // Store original cooking total time
-            int originalTotalTime = COOKING_TOTAL_TIME_FIELD.getInt(furnace);
-            
-            // Temporarily set it to 1/4 for this tick
-            if (originalTotalTime > 0) {
-                COOKING_TOTAL_TIME_FIELD.setInt(furnace, Math.max(1, originalTotalTime / 4));
-            }
-            
-            AbstractFurnaceBlockEntity.serverTick(level, pos, state, furnace);
-            
-            // Restore the original total time
-            COOKING_TOTAL_TIME_FIELD.setInt(furnace, originalTotalTime);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        AbstractFurnaceBlockEntity.serverTick(level, pos, state, furnace);
     }
 }
